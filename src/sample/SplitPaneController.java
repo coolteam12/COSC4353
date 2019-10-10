4162caca5367d7528887ff2e26713e7936887d5e
Alexander Bien <alexanderpbien@gmail.com>
2019-10-10 01:34:50 -0500

    Semi-Working Compiler + Word Count
    Combination of Frank and Alex's work

--- src/sample/SplitPaneController.java
+++ src/sample/SplitPaneController.java
@@ -82,6 +82,28 @@ public class SplitPaneController {
     }
 
     @FXML
+    private void getWordCount(MouseEvent event){
+
+        if(area!= null){
+            String text = area.getText();
+            String[] lineArray = text.split("\n");
+            int forCount = 0, whileCount = 0, ifCount = 0, elseCount = 0;
+            for(int i = 0; i < lineArray.length-1; i++ ){
+                for(int j = 0; j < lineArray[i].length()-1; j++){
+                    if(lineArray[i].charAt(j)=='i' && lineArray[i].charAt(j+1) == 'f'&& !Character.toString(lineArray[i].charAt(j-1)).matches("[a-z?]")&& !Character.toString(lineArray[i].charAt(j+2)).matches("[a-z?]")&& !Character.toString(lineArray[i].charAt(j-1)).matches("[A-Z?]")&& !Character.toString(lineArray[i].charAt(j+2)).matches("[A-Z?]") ){ifCount++;}
+                    else if(lineArray[i].charAt(j)=='w' && lineArray[i].charAt(j+1) == 'h' &&lineArray[i].charAt(j+2)=='i' && lineArray[i].charAt(j+3) == 'l' &&lineArray[i].charAt(j+4)=='e'&& !Character.toString(lineArray[i].charAt(j-1)).matches("[a-z?]")&& !Character.toString(lineArray[i].charAt(j+5)).matches("[a-z?]")&& !Character.toString(lineArray[i].charAt(j-1)).matches("[A-Z?]")&& !Character.toString(lineArray[i].charAt(j+5)).matches("[A-Z?]")){whileCount++;}
+                    else if(lineArray[i].charAt(j)=='f' && lineArray[i].charAt(j+1) == 'o' &&lineArray[i].charAt(j+2)=='r'&& !Character.toString(lineArray[i].charAt(j-1)).matches("[a-z?]")&& !Character.toString(lineArray[i].charAt(j+3)).matches("[a-z?]")&& !Character.toString(lineArray[i].charAt(j-1)).matches("[A-Z?]")&& !Character.toString(lineArray[i].charAt(j+3)).matches("[A-Z?]")){forCount++;}
+                    else if(lineArray[i].charAt(j)=='e' && lineArray[i].charAt(j+1) == 'l' &&lineArray[i].charAt(j+2)=='s' && lineArray[i].charAt(j+3) == 'e'&& !Character.toString(lineArray[i].charAt(j-1)).matches("[a-z?]")&& !Character.toString(lineArray[i].charAt(j+4)).matches("[a-z?]")&& !Character.toString(lineArray[i].charAt(j-1)).matches("[A-Z?]")&& !Character.toString(lineArray[i].charAt(j+4)).matches("[A-Z?]")){elseCount++;}
+                }
+            }
+            System.out.println("Amount of for: " + forCount);
+            System.out.println("Amount of while: " + whileCount);
+            System.out.println("Amount of if: " + ifCount);
+            System.out.println("Amount of else: " + elseCount);
+        }
+    }
+
+    @FXML
     public void toggleFileViewer(KeyEvent event) {
         if (toggleCombination.match(event)) {
             toggleFileViewer = !toggleFileViewer;
