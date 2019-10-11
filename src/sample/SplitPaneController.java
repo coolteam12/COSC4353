9b34e58af1c3922e0b179e3a0dff278b523b29f2
Alexander Bien <alexanderpbien@gmail.com>
2019-10-10 20:44:43 -0500

    Finalized Word Counter
    Changed the way in which I grabbed the words so that it cannot go out of bounds

--- src/sample/SplitPaneController.java
+++ src/sample/SplitPaneController.java
@@ -25,6 +25,10 @@ import org.reactfx.Subscription;
 
 import java.lang.reflect.Method;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SplitPaneController {
 
@@ -150,32 +154,35 @@ public class SplitPaneController {
 
     //When the button "Word Count" is pressed, this function is called to do the calculations
     @FXML
-    private void getWordCount(MouseEvent event){
+    private void getWordCount(ActionEvent event){
 
         //As long as the text area is filled, it will run correctly
         if(area!= null){
 
             String text = area.getText();
-            String[] lineArray = text.split("\n");
+            System.out.println(text);
             int forCount = 0, whileCount = 0, ifCount = 0, elseCount = 0;
+            Pattern forPattern = Pattern.compile("for");
+            Pattern whilePattern = Pattern.compile("while");
+            Pattern ifPattern = Pattern.compile("if");
+            Pattern elsePattern = Pattern.compile("else");
+            Matcher forMatcher = forPattern.matcher(text);
+            Matcher whileMatcher = whilePattern.matcher(text);
+            Matcher ifMatcher = ifPattern.matcher(text);
+            Matcher elseMatcher = elsePattern.matcher(text);
 
             //Each line of the code is read as an array
-            for(int i = 0; i < lineArray.length-1; i++ ){
-                //Each character of string of lines are then further parsed
-                for(int j = 0; j < lineArray[i].length()-1; j++){
-                    //looks for the specific key words "for","else","while",and "if" while ignoring other words that may happen to just have those words in them like Afford
-                    if(lineArray[i].charAt(j)=='i' && lineArray[i].charAt(j+1) == 'f'&& !Character.toString(lineArray[i].charAt(j-1)).matches("[a-z?]")&& !Character.toString(lineArray[i].charAt(j+2)).matches("[a-z?]")&& !Character.toString(lineArray[i].charAt(j-1)).matches("[A-Z?]")&& !Character.toString(lineArray[i].charAt(j+2)).matches("[A-Z?]") ){ifCount++;}
-                    else if(lineArray[i].charAt(j)=='w' && lineArray[i].charAt(j+1) == 'h' &&lineArray[i].charAt(j+2)=='i' && lineArray[i].charAt(j+3) == 'l' &&lineArray[i].charAt(j+4)=='e'&& !Character.toString(lineArray[i].charAt(j-1)).matches("[a-z?]")&& !Character.toString(lineArray[i].charAt(j+5)).matches("[a-z?]")&& !Character.toString(lineArray[i].charAt(j-1)).matches("[A-Z?]")&& !Character.toString(lineArray[i].charAt(j+5)).matches("[A-Z?]")){whileCount++;}
-                    else if(lineArray[i].charAt(j)=='f' && lineArray[i].charAt(j+1) == 'o' &&lineArray[i].charAt(j+2)=='r'&& !Character.toString(lineArray[i].charAt(j-1)).matches("[a-z?]")&& !Character.toString(lineArray[i].charAt(j+3)).matches("[a-z?]")&& !Character.toString(lineArray[i].charAt(j-1)).matches("[A-Z?]")&& !Character.toString(lineArray[i].charAt(j+3)).matches("[A-Z?]")){forCount++;}
-                    else if(lineArray[i].charAt(j)=='e' && lineArray[i].charAt(j+1) == 'l' &&lineArray[i].charAt(j+2)=='s' && lineArray[i].charAt(j+3) == 'e'&& !Character.toString(lineArray[i].charAt(j-1)).matches("[a-z?]")&& !Character.toString(lineArray[i].charAt(j+4)).matches("[a-z?]")&& !Character.toString(lineArray[i].charAt(j-1)).matches("[A-Z?]")&& !Character.toString(lineArray[i].charAt(j+4)).matches("[A-Z?]")){elseCount++;}
-                }
-            }
+            while(forMatcher.find()){forCount++;}
+            while(whileMatcher.find()){whileCount++;}
+            while(ifMatcher.find()){ifCount++;}
+            while(elseMatcher.find()){elseCount++;}
+
             String message = ("Amount of for: " + forCount + " Amount of while: " + whileCount + " Amount of if: " + ifCount + " Amount of else: " + elseCount);
             lblTest.setText(message);
-            //Displayes the word count as a PopupBox
-            btnWordCount.setOnAction(e-> PopupBox.display("Word Counter", message));
-        }else{
-            btnWordCount.setOnAction(e-> PopupBox.display("Word Counter", "Please open up a file to use the Word Counter."));
+            //Displays the word count as a PopupBox
+            //btnWordCount.setOnAction(e-> PopupBox.display("Word Counter", message));
+        }else {
+            //btnWordCount.setOnAction(e -> PopupBox.display("Word Counter", "Please open up a file to use the Word Counter."));
         }
     }
     @FXML
