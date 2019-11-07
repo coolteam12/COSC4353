f77a727f7897d5776d74f09e5452bfb54ba0bfd5
ryantorecampo <ryan.torrecampo27@gmail.com>
2019-11-07 17:34:24 -0600

    clean up

--- src/sample/CompilerUtils.java
+++ src/sample/CompilerUtils.java
@@ -4,10 +4,10 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-public class CompilerUtils {
+class CompilerUtils extends ClassLoader{
 
     //Helps print what comes out of the programs when ran/compiled
-    public static void printLines (String cmd, InputStream ins) throws Exception{
+    private static void printLines(String cmd, InputStream ins) throws Exception{
         String line = null;
         BufferedReader in = new BufferedReader(new InputStreamReader(ins));
         while((line = in.readLine()) != null){
@@ -15,8 +15,17 @@ public class CompilerUtils {
         }
     }
     //Runs the commands needed to compile/run
-    public static void runProcess(String command) throws Exception{
+    static void runProcess(String command) throws Exception{
         Process pro = Runtime.getRuntime().exec(command);
+        final StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+        
+//        Class<?> cls = Class.forName("");
+//        System.out.println("Class Name: " + cls.getName());
+//        System.out.println("Package Name: " + cls.getPackage());
+//        Method[] methods = cls.getDeclaredMethods();
+//        for(Method method : methods)  {
+//            System.out.println(method.getName());
+//        }
         printLines("stdout:",pro.getInputStream());
         printLines("stderr:", pro.getErrorStream());
         pro.waitFor();
